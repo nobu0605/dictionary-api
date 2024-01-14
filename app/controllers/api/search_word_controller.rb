0a7d612d70230e307d85bf78410ba4bdbd96a17c
Takanobu Suzuki <nobublack555@gmail.com>
2024-01-14 23:25:40 +0900

    feature/dictionary

--- app/controllers/api/search_word_controller.rb
+++ app/controllers/api/search_word_controller.rb
@@ -1,14 +1,35 @@
+# typed: true
+
 class Api::SearchWordController < ApplicationController
   before_action :authenticate_api_user!
   require "net/http"
+  extend T::Sig
 
+  sig {void}
   def search_word
-    url = "#{WORDS_API_ENDPOINT}/words/#{params[:word]}/typeOf"
-    uri = URI.parse(url)
-    headers = { "X-RapidAPI-Key" => Rails.application.credentials.words_api[:api_key] }
-    response = Net::HTTP.get(uri,headers)
-    JSON.parse(response)
-    render json: response
+    begin
+      headers = { "X-RapidAPI-Key" => Rails.application.credentials.words_api[:api_key] }
+
+      word_url = T.let("#{WORDS_API_ENDPOINT}/words/#{params[:word]}", String)
+      word_uri = URI.parse(word_url)
+      word_res = Net::HTTP.get(word_uri,headers)
+      word = JSON.parse(word_res)
+
+      if word["success"] == false
+        return render status: "404", json: { message: "Word not found" }
+      end
+
+      examples_url = T.let("#{WORDS_API_ENDPOINT}/words/#{params[:word]}/examples", String)
+      examples_uri = URI.parse(examples_url)
+      examples_res = Net::HTTP.get(examples_uri,headers)
+
+      render status: "200", json: {
+        word: JSON.parse(word_res),
+        examples: JSON.parse(examples_res)
+      }
+    rescue => e
+      render status: "500", json: { message: "Internal Server Error" }
+    end
   end
 
 end
